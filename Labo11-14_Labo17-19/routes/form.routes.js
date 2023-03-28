327771d2ed23588948df1f00702ea7205d8c39ff
Ramona Nájera Fuentes <91426086+RamonaNF@users.noreply.github.com>
2023-03-28 09:12:58 -0600

    Delete Labo11-14_Labo17-19 directory

--- Labo11-14_Labo17-19/routes/form.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const formController = require('../controllers/form.controller');
-const router = express.Router();
-
-router.get('/', formController.getForm);
-router.post('/', formController.postForm);
-
-module.exports = router;
